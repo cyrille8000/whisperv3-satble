4bd851ce298cbe45c4fd0cef7c2473a7ec9ad281
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-23 20:55:00 +0100

    Update main.py
    delete comma(,)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import stable_whisper
 model = stable_whisper.load_model('base')
-result = model.transcribe('episode3.wav', regroup='cm_sp=.* /。/?/？/,* /，_sg=.5_mg=.3+3_sp=.* /。/?/？', demucs=True, vad=True, mel_first=True)
+result = model.transcribe('episode3.wav', regroup='cm_sp=.* /。/?/？ /，_sg=.5_mg=.3+3_sp=.* /。/?/？', demucs=True, vad=True, mel_first=True)
 result.to_srt_vtt('audio.srt', segment_level=True, word_level=False)
