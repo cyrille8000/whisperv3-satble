9dc3e66d72f9ad4d4ddf8ee9c970f6222a4627a9
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-23 20:38:47 +0100

    Update main.py
    regroup word by punctuation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import stable_whisper
 model = stable_whisper.load_model('base')
-result = model.transcribe('segment6.wav', regroup=True)
+result = model.transcribe('segment6.wav', regroup='cm_sp=.* /。/?/？/,* /，_sg=.5_mg=.3+3_sp=.* /。/?/？')
 result.to_srt_vtt('audio.srt')
