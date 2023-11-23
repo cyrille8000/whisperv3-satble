c959a02572cbf8ef78f0628d52a210bea99abc2b
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-23 22:05:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import stable_whisper
 model = stable_whisper.load_model('base')
-result = model.transcribe('episode3.wav', regroup='sp=.* /。/!/?/？+1', demucs=True, vad=True, mel_first=True)
+result = model.transcribe('segment2.wav', regroup='sp=.* /。/!/?/？+1', demucs=True, vad=True, mel_first=True)
 result.to_srt_vtt('audio.srt', segment_level=True, word_level=False)
