9c28b02f59f65c03ace85eb07ed7dd265995161a
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-23 20:36:45 +0100

    Update main.py
    regroup word

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import stable_whisper
 model = stable_whisper.load_model('base')
-result = model.transcribe('segment6.wav')
+result = model.transcribe('segment6.wav', regroup=True)
 result.to_srt_vtt('audio.srt')
