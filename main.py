759cd990dae14a092a30db5cc14bb9027e43d5ed
cyrille800 <armand-cyrille.takougang_nzeukou@edu.devinci.fr>
2023-11-24 07:12:56 +0000

    clean repo

--- main.py
+++
@@ -1,4 +0,0 @@
-import stable_whisper
-model = stable_whisper.load_model('base')
-result = model.transcribe('segment2.wav', regroup='sp=.* /。/!/?/？+1', demucs=True, vad=True, mel_first=True)
-result.to_srt_vtt('audio.srt', segment_level=True, word_level=False)
