1a904d8aeab1f9abbf1fba63721d837dcfae1585
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-26 03:26:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import requests
 
 url = "http://localhost:5000/predictions"
 
-# Création du dictionnaire des paramètres
-with open('segment2.wav', 'rb') as file:
-    params = {
-        "input": {
-            "vad": True,
-            "demucs": True,
-            "regroup": "sp=.* /。/!/?/？+1",
-            "mel_first": True,
-            "audio_file": file,
-            "model_path": "large-v3",
-            "word_level": False,
-            "model_device": "cuda",
-            "segment_level": True
-        }
-    }
+# Chemin vers le fichier à envoyer
+file_path = "segment2.wav"
 
-    # Envoyer la requête POST avec les paramètres et le fichier
-    response = requests.post(url, json=params)
+# Paramètres supplémentaires à envoyer avec le fichier
+params = {
+    "vad": True,
+    "demucs": True,
+    "regroup": "sp=.* /。/!/?/？+1",
+    "mel_first": True,
+    "model_path": "large-v3",
+    "word_level": False,
+    "model_device": "cuda",
+    "segment_level": True
+}
+
+# Ouvrir le fichier en mode binaire
+with open(file_path, 'rb') as file:
+    # Créer un dictionnaire pour les fichiers
+    files = {'audio_file': (file_path, file)}
+
+    # Envoyer la requête POST avec le fichier et les paramètres
+    response = requests.post(url, data=params, files=files)
+
+    # Afficher la réponse du serveur
     print(response.text)
