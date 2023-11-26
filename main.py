d0e3960f6c7bf045ce9f9eef349c5d53dcbedc5f
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-26 03:34:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import requests
-import base64
 
 url = "http://localhost:5000/predictions"
 
 # Chemin vers le fichier à envoyer
 file_path = "segment2.wav"
 
-# Lire et encoder le fichier en base64
-with open(file_path, 'rb') as file:
-    encoded_file = base64.b64encode(file.read()).decode('utf-8')
-
-# Préparer les données JSON avec le fichier encodé
-data = {
+# Paramètres supplémentaires à envoyer avec le fichier
+params = {
     "vad": True,
     "demucs": True,
     "regroup": "sp=.* /。/!/?/？+1",
     "mel_first": True,
-    "audio_file": encoded_file,
     "model_path": "large-v3",
     "word_level": False,
     "model_device": "cuda",
     "segment_level": True
 }
 
-# Envoyer la requête POST avec les données JSON
-response = requests.post(url, json={'input': data})
-print(response.text)
+# Ouvrir le fichier en mode binaire
+with open(file_path, 'rb') as file:
+    # Créer un dictionnaire pour les fichiers
+    files = {'audio_file': (file_path, file)}
+
+    # Envoyer la requête POST avec le fichier et les paramètres
+    response = requests.post(url, data=params, files=files)
+
+    # Afficher la réponse du serveur
+    print(response.text)
