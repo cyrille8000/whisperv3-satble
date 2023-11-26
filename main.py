6c68801b5275bf7fb839445904f2ce306f361eff
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-26 03:23:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import requests
 url = "http://localhost:5000/predictions"
 
 # Création du dictionnaire des paramètres
-params = {
-    "input": {
-        "vad": True,
-        "demucs": True,
-        "regroup": "sp=.* /。/!/?/？+1",
-        "mel_first": True,
-        "audio_file": 'segment2.wav',
-        "model_path": "large-v3",
-        "word_level": False,
-        "model_device": "cuda",
-        "segment_level": True
+with open('segment2.wav', 'rb') as file:
+    params = {
+        "input": {
+            "vad": True,
+            "demucs": True,
+            "regroup": "sp=.* /。/!/?/？+1",
+            "mel_first": True,
+            "audio_file": file,
+            "model_path": "large-v3",
+            "word_level": False,
+            "model_device": "cuda",
+            "segment_level": True
+        }
     }
-}
 
-# Ajouter le fichier en tant que partie multipart/form-data
-files = {
-    'audio_file': open('segment2.wav', 'rb')
-}
-
-# Envoyer la requête POST avec les paramètres et le fichier
-response = requests.post(url, json=params)
-print(response.text)
+    # Envoyer la requête POST avec les paramètres et le fichier
+    response = requests.post(url, json=params)
+    print(response.text)
