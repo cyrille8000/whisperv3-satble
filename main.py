b4085c453c33c7d62100660bcc655224e6ffa12b
cyrille8000 <151868109+cyrille8000@users.noreply.github.com>
2023-11-26 03:09:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,27 @@
-from cog import BasePredictor, Path, Input
-import stable_whisper
-import torch
-import json
-import re
+import requests
 
-def parse_srt_file(filepath):
-    """Parse un fichier SRT et retourne les données dans un format structuré."""
-    with open(filepath, 'r', encoding='utf-8') as file:
-        content = file.read()
-        
-    # Regex pour trouver les temps et les textes
-    pattern = re.compile(r'(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})\s*(.*?)(?=\n\n|\Z)', re.DOTALL)
-    matches = pattern.findall(content)
+url = "http://localhost:5000/predictions"
 
-    segments = []
-    for start, end, text in matches:
-        segment = {
-            "text": text.strip().replace('\n', ' '),
-            "start": start,
-            "end": end
-        }
-        segments.append(segment)
+# Création du dictionnaire des paramètres
+params = {
+    "input": {
+        "vad": True,
+        "demucs": True,
+        "regroup": "sp=.* /。/!/?/？+1",
+        "mel_first": True,
+        "audio_file": (None, 'segment2.wav'),
+        "model_path": "large-v3",
+        "word_level": False,
+        "model_device": "cuda",
+        "segment_level": True
+    }
+}
 
-    return segments
+# Ajouter le fichier en tant que partie multipart/form-data
+files = {
+    'audio_file': open('segment2.wav', 'rb')
+}
 
-
-device = "cuda"
-
-# Load the model based on the 'size' argument
-model = stable_whisper.load_model("large-v2" , download_root="whisper-cache", device=device)
-
-# Perform the transcription
-result = model.transcribe("segment2.wav", regroup='sp=.* /。/!/?/？+1', demucs=True, vad=True, mel_first=True)
-
-result.to_srt_vtt('./audio.srt', segment_level=True, word_level=False)
-
-srt_data = parse_srt_file('./audio.srt')
-
-# Convertir en JSON
-json_data = json.dumps({"segmentation": srt_data}, ensure_ascii=False, indent=4)
-
-print(json_data)
+# Envoyer la requête POST avec les paramètres et le fichier
+response = requests.post(url, json=params, files=files)
+print(response)
